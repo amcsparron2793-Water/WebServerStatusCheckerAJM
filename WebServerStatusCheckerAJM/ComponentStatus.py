3af432ae4bbca9de48b8f63770ff24fd44508c64
Andrew McSparron <Amcsparron@albanyny.gov>
2024-10-10 15:16:45 -0400

    Refactor status checks into ComponentStatus class
    Moved status-related properties and methods from WebServerStatusCheck to a new ComponentStatus class. This change improves code modularity and separation of concerns, making the codebase easier to maintain and extend.

---
+++ WebServerStatusCheckerAJM/ComponentStatus.py
@@ -0,0 +1,94 @@
+from re import fullmatch
+
+import requests
+
+
+class ComponentStatus:
+    LOGGER = None
+
+    def __init__(self):
+
+        self._server_status = None
+        self._page_status = None
+        self._machine_status = None
+        self._local_machine_ping_host = None
+        self._local_machine_status = None
+
+    @property
+    def server_status(self):
+        return self._server_status
+
+    @server_status.getter
+    def server_status(self):
+        try:
+            r = requests.get(self.server_full_address)
+            self._server_status = True
+        except requests.exceptions.ConnectionError as e:
+            self._server_status = False
+
+        return self._server_status
+
+    @property
+    def page_status(self):
+        return self._page_status
+
+    @page_status.getter
+    def page_status(self):
+        self._page_status = False
+        if self.server_status:
+            try:
+                r = requests.get(self.server_full_address)
+                if r.ok:
+                    self._page_status = True
+            except requests.exceptions.ConnectionError as e:
+                self._page_status = False
+        return self._page_status
+
+    @property
+    def machine_status(self):
+        return self._machine_status
+
+    @machine_status.getter
+    def machine_status(self):
+        self._machine_status = False
+        if self.local_machine_status:
+            if self.ping():
+                self._machine_status = True
+            else:
+                self._machine_status = False
+        return self._machine_status
+
+    @property
+    def local_machine_ping_host(self):
+        return self._local_machine_ping_host
+
+    @local_machine_ping_host.setter
+    def local_machine_ping_host(self, value):
+        try:
+            if fullmatch(r'^((\d{1,3}\.){3}(\d{1,3}))', value):
+                self._local_machine_ping_host = value
+            else:
+                try:
+                    raise AttributeError('must be a valid PLAIN IP address in the form of ddd.ddd.ddd'
+                                         ' or any other valid octet')
+                except AttributeError as e:
+                    self.LOGGER.error(e, exc_info=True)
+                    raise e
+        except TypeError as e:
+            self.LOGGER.error(e, exc_info=True)
+            raise e
+
+    @property
+    def local_machine_status(self):
+        return self._local_machine_status
+
+    @local_machine_status.getter
+    def local_machine_status(self):
+        """ pings a very high uptime server (one of googles public DNS servers,
+         or a given server through self.local_machine_ping_host)
+        to see if the machine on which the code is running, has any network connectivity at all."""
+        if self.ping(host=self.local_machine_ping_host):
+            self._local_machine_status = True
+        else:
+            self._local_machine_status = False
+        return self._local_machine_status
--- WebServerStatusCheckerAJM/WebServerStatusCheckerAJM.py
+++ WebServerStatusCheckerAJM/WebServerStatusCheckerAJM.py
@@ -11,7 +11,6 @@ import requests
 from time import sleep
 import platform
 import subprocess
-from re import fullmatch
 
 from typing import List, Dict
 from EasyLoggerAJM import EasyLogger
@@ -21,6 +20,7 @@ import ctypes
 import winsound
 from WebServerStatusCheckerAJM._version import __version__
 from WebServerStatusCheckerAJM.ServerAddressPort import ServerAddressPort
+from WebServerStatusCheckerAJM.ComponentStatus import ComponentStatus
 
 
 class WebServerEasyLogger(EasyLogger):
@@ -31,7 +31,7 @@ class WebServerEasyLogger(EasyLogger):
         raise NotImplementedError("not implemented yet.")
 
 
-class WebServerStatusCheck(ServerAddressPort):
+class WebServerStatusCheck(ServerAddressPort, ComponentStatus):
     LOGGER = EasyLogger.UseLogger().logger
     WINAPI_MSG_BOX_STYLES = {
         'OK': 0,
@@ -163,86 +163,6 @@ class WebServerStatusCheck(ServerAddressPort):
     def just_started(self, value: bool):
         self._just_started = value
 
-    # TODO: move this to a Status Subclass?
-    @property
-    def server_status(self):
-        return self._server_status
-
-    @server_status.getter
-    def server_status(self):
-        try:
-            r = requests.get(self.server_full_address)
-            self._server_status = True
-        except requests.exceptions.ConnectionError as e:
-            self._server_status = False
-
-        return self._server_status
-
-    @property
-    def page_status(self):
-        return self._page_status
-
-    @page_status.getter
-    def page_status(self):
-        self._page_status = False
-        if self.server_status:
-            try:
-                r = requests.get(self.server_full_address)
-                if r.ok:
-                    self._page_status = True
-            except requests.exceptions.ConnectionError as e:
-                self._page_status = False
-        return self._page_status
-
-    @property
-    def machine_status(self):
-        return self._machine_status
-
-    @machine_status.getter
-    def machine_status(self):
-        self._machine_status = False
-        if self.local_machine_status:
-            if self.ping():
-                self._machine_status = True
-            else:
-                self._machine_status = False
-        return self._machine_status
-
-    @property
-    def local_machine_ping_host(self):
-        return self._local_machine_ping_host
-
-    @local_machine_ping_host.setter
-    def local_machine_ping_host(self, value):
-        try:
-            if fullmatch(r'^((\d{1,3}\.){3}(\d{1,3}))', value):
-                self._local_machine_ping_host = value
-            else:
-                try:
-                    raise AttributeError('must be a valid PLAIN IP address in the form of ddd.ddd.ddd'
-                                         ' or any other valid octet')
-                except AttributeError as e:
-                    self.LOGGER.error(e, exc_info=True)
-                    raise e
-        except TypeError as e:
-            self.LOGGER.error(e, exc_info=True)
-            raise e
-
-    @property
-    def local_machine_status(self):
-        return self._local_machine_status
-
-    @local_machine_status.getter
-    def local_machine_status(self):
-        """ pings a very high uptime server (one of googles public DNS servers,
-         or a given server through self.local_machine_ping_host)
-        to see if the machine on which the code is running, has any network connectivity at all."""
-        if self.ping(host=self.local_machine_ping_host):
-            self._local_machine_status = True
-        else:
-            self._local_machine_status = False
-        return self._local_machine_status
-
     @property
     def full_status_string(self):
         return self._full_status_string
@@ -276,6 +196,7 @@ class WebServerStatusCheck(ServerAddressPort):
 
         return self._full_status_string
 
+    # TODO: TitlesNames subclass?
     @property
     def html_title(self):
         return self._html_title
